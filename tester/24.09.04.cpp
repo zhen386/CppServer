9ab1189a6c16b4cabbc3b8aacc36f2ebf9ca9fed
zhen <MangoIce386@gmail.com>
2024-09-04 16:04:32 +0800

    24.09.04cpp学习

--- tester/24.09.04.cpp
+++ tester/24.09.04.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 
 
-int& test03(){
-	static int a = 10;
-    return a;
-}
+class Person{
+
+public:
+  int age;
+  int* height;
+ 
+  Person(int m_age, int* m_height){
+	age = m_age;
+    height = m_height;
+  }
+  
+  ~Person(){
+    if(height != NULL){
+      delete height;
+      height = NULL;
+    }
+  }
+    
+};
 
 int main(){
-
-    int &ref3 = test03();
-    std::cout << ref3 << std::endl; //ref3 = 10
-
-    test03() = 1000;	//对返回的引用进行的赋值操作
-    std::cout << ref3 << std::endl; //ref3 = 1000 
-
-    return 0;
-
+  Person p1 = Person(18, new int(160));
+  Person p2 = Person(p1);
+  std::cout << "p1的年龄是: " << p1.age << std::endl;
+  std::cout << "p1的身高是: " << *p1.height << std::endl;
+  return 0;
 }
